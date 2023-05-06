f6317b30c29384ed05375f9046e3344dad0d122b
Celety <116971721+Celety@users.noreply.github.com>
2023-05-07 01:18:59 +0200

    Added video background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import backgroundVideo from "./background.mp4";
 
 import SearchEngine from "./SearchEngine";
 import Footer from "./Footer";
@@ -7,6 +8,9 @@ import Footer from "./Footer";
 export default function App() {
   return (
     <div className="App">
+      <video playsInline autoPlay muted loop id="backgroundVideo">
+        <source src={backgroundVideo} type="video/mp4" />{" "}
+      </video>
       <div className="Content">
         <SearchEngine defaultCity="WARSAW" />
         <Footer />
